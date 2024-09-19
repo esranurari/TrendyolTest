c98dd17f51cc6505e500d9eb5bdb78ac610f24bc
Hasan Ali DONMEZ <hasanali12091@gmail.com>
2024-09-19 21:23:31 +0300

    v324234

--- src/test/java/trendyol/tests/C01.java
+++ src/test/java/trendyol/tests/C01.java
@@ -18,6 +18,7 @@ public class C01 {
     //githup
     //21.30
     //asjkldfaksdf
+    //esra
     // 1. https://www.trendyol.com/ adresine gidin
     // 2. Ana sayfanın açıldığını kontrol edin.
     // 3. Yanlış bilgilerle siteye giriş yapılmaya çalışılıyor.
